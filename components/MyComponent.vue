69fd83ce6b82f752a527ffa3f7cc024293a2aad0
EKorenovsky <e_korenovsky@mail.ru>
2020-05-18 23:09:48 +0300

    test

---
+++ components/MyComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-btn
+      class="mr-3"
+      elevation="1"
+      fab
+      small
+      @click="onClick"
+    >
+      <v-icon v-if="value">
+        mdi-view-quilt
+      </v-icon>
+
+      <v-icon v-else>
+        mdi-dots-vertical
+      </v-icon>
+    </v-btn>
+</template>
+
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { VHover, VListItem } from 'vuetify/lib';
+import { mapState, mapMutations } from 'vuex';
+
+@Component
+export default class MyComponent extends Vue {
+  message: string = 'This is a message';
+  value: boolean = false;
+  onClick (): void {
+    window.alert(this.message);
+  }
+  
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <h1 class="header">Nuxt TypeScript Starter</h1>
+    <div class="cards">
+      <Card
+        v-for="person in people"
+        :key="person.id"
+        :person="person"
+      ></Card>
+    </div>
+    <div>
+      <my-component/>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import {Component,Vue}  from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import { Person } from "~/types";
+import Card from "~/components/Card.vue";
+import MyComponent from "~/components/MyComponent.vue";
+
+
+@Component({
+  components: {
+    Card, MyComponent
+  }
+})
+export default class extends Vue {
+  @State people!: Person[]
+}
+</script>
+
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
